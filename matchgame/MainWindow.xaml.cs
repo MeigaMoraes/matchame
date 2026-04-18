a650773d089172fe2331dbf91917f637f7541ed0
Victor <moraesmeiga@gmail.com>
2026-04-18 01:50:00 -0300

    A busca pelo par foi adicionada. Cronometro adicionado, porem com um bub

--- matchgame/MainWindow.xaml.cs
+++ matchgame/MainWindow.xaml.cs
@@ -15,15 +15,38 @@ using System.Windows.Shapes;
 
 namespace matchgame
 {
-    /// <summary>
-    /// Interação lógica para MainWindow.xam
-    /// </summary>
+
+    using System.Windows.Threading;
+
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;//armazena a quantidade de pares encontrados
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
+
+
             SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            TimeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if(matchesFound == 8){
+                timer.Stop();
+                TimeTextBlock.Text = TimeTextBlock.Text + " - play again?";
+            }
+
         }
 
         private void SetUpGame()
@@ -49,6 +72,42 @@ namespace matchgame
                 animalEmoji.RemoveAt(index); //remove o emoji atual, sendo assim impedindo de aparecer o mesmo emoji mais de duas vezes (duas vezes pq ainda tem o outro do par para aparecer)
 
                 
+            }
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if(findingMatch == false) // o bloco controla se o jogador clicou ou não no primeiro animal do par, se clicou ele esconde o animal e redefinaa findindMatch pra true
+            {
+                textBlock.Visibility = Visibility.Hidden; 
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+
+            }
+            else if(textBlock.Text == lastTextBlockClicked.Text) // se o segundo animal clicado for igual ao primeiro, ele apaga o animal e redefine findingMatch pra false
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible; // se o animal clicado nao for igual ao primeiro ele então deixa o primeiro animal clicado visivel e redefine findingMatch
+                findingMatch = false;
+            }
+
+
+
+        }
+
+        private void TimeTextBlock_mouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(matchesFound == 8)
+            {
+                SetUpGame();
             }
         }
     }
